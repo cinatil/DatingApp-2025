2e37c75329e2db9a8f0eadd8d800104bc5e90444
Hanna <hhusken@hotmail.com>
2025-04-15 16:57:05 +0200

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected DataContext()
+        {
+        }
+    }
+}
