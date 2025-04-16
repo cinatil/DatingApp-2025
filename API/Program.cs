d450ba10b17cc369e19ef6d9c20283369a135d5c
Hanna <hhusken@hotmail.com>
2025-04-16 11:23:15 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -3,17 +3,31 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// ✅ Define CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapControllers();
+// ✅ Middleware order matters!
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy"); // 🟢 Apply here BEFORE controllers are mapped
+
+app.MapControllers(); // 🟢 This must come AFTER CORS
 
 app.Run();
